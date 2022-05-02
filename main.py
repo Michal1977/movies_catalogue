631e66666d17da8709de81ff3164febb12775e31
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-05-02 12:44:45 +0200

    added_lists

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from flask import Flask, render_template
 
-from movies_project.movies_catalogue.movies_API import get_popular_movies, get_configuration, get_poster_url
+from movies_API import get_backdrop_url, get_configuration, get_movie_credits, get_movie_details, get_popular_movies, \
+    get_poster_url, get_upcoming_movies
 
 app = Flask(__name__)
 
 
-#@app.route('/')
-#def homepage():
-#    return render_template("index.html")
+@app.context_processor
+def utility_processor():
+    def get_poster_url_preprocesor(conf, poster_api_path, size="w342"):
+        return get_poster_url(conf, poster_api_path, size)
+
+    def get_backdrop_url_preprocesor(conf, backdrop_api_path, size="w300"):
+        return get_backdrop_url(conf, backdrop_api_path, size)
+
+    return dict(get_poster_url_in_html=get_poster_url_preprocesor,
+                get_backdrop_url_in_html=get_backdrop_url_preprocesor)
 
 
 @app.route('/')
 def homepage():
     movies = get_popular_movies()
     config = get_configuration()
-    a = get_poster_url(config, "poster", "w185")
     return render_template("homepage.html", movies=movies, config=config)
+
+
+@app.route('/upcoming')
+def upcoming():
+    movies = get_upcoming_movies()
+    config = get_configuration()
+    return render_template("homepage.html", movies=movies, config=config)
+
+
+@app.route('/movies/<movie_id>')
+def one_movie(movie_id):
+    config = get_configuration()
+    movie_details = get_movie_details(movie_id)
+    credits = get_movie_credits(movie_id)
+    return render_template("movie.html", movie=movie_details, config=config, credits=credits)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- movies_API.py
+++ movies_API.py
@@ -1,16 +1,6 @@
 import requests
 
 
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzY4ZDMxNDhjOTU3ZGIxMmQ5MmI0MmFiOTI0YjRiNCIsInN1YiI6IjYyNjkzMTE4NWFiODFhMTMyZTFmOTM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTcpJnh0eCNiXJlTdIBLqNF6qcqeIm0_Li2fUKWtxMk"
-    headers = {
-        "Authorization": f"Bearer {api_token}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()['results']
-
-
 def get_configuration():
     endpoint = "https://api.themoviedb.org/3/configuration"
     api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzY4ZDMxNDhjOTU3ZGIxMmQ5MmI0MmFiOTI0YjRiNCIsInN1YiI6IjYyNjkzMTE4NWFiODFhMTMyZTFmOTM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTcpJnh0eCNiXJlTdIBLqNF6qcqeIm0_Li2fUKWtxMk"
@@ -21,10 +11,56 @@ def get_configuration():
     return response.json()['images']
 
 
-def get_poster_url(conf, poster_api_path, size="w342"):
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzY4ZDMxNDhjOTU3ZGIxMmQ5MmI0MmFiOTI0YjRiNCIsInN1YiI6IjYyNjkzMTE4NWFiODFhMTMyZTFmOTM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTcpJnh0eCNiXJlTdIBLqNF6qcqeIm0_Li2fUKWtxMk"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()['results']
 
+
+def get_upcoming_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/upcoming"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzY4ZDMxNDhjOTU3ZGIxMmQ5MmI0MmFiOTI0YjRiNCIsInN1YiI6IjYyNjkzMTE4NWFiODFhMTMyZTFmOTM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTcpJnh0eCNiXJlTdIBLqNF6qcqeIm0_Li2fUKWtxMk"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()['results']
+
+
+def get_movie_details(id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{id}"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzY4ZDMxNDhjOTU3ZGIxMmQ5MmI0MmFiOTI0YjRiNCIsInN1YiI6IjYyNjkzMTE4NWFiODFhMTMyZTFmOTM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTcpJnh0eCNiXJlTdIBLqNF6qcqeIm0_Li2fUKWtxMk"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_movie_credits(id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{id}/credits"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzY4ZDMxNDhjOTU3ZGIxMmQ5MmI0MmFiOTI0YjRiNCIsInN1YiI6IjYyNjkzMTE4NWFiODFhMTMyZTFmOTM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTcpJnh0eCNiXJlTdIBLqNF6qcqeIm0_Li2fUKWtxMk"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_poster_url(conf, poster_api_path, size="w342"):
     base_url = conf['secure_base_url']
     if size not in conf['poster_sizes']:
         size = conf['poster_sizes'][0]
 
     return f"{base_url}{size}{poster_api_path}"
+
+
+def get_backdrop_url(conf, backdrop_api_path, size="w300"):
+    base_url = conf['secure_base_url']
+    if size not in conf['backdrop_sizes']:
+        size = conf['backdrop_sizes'][0]
+    return f"{base_url}{size}{backdrop_api_path}"
